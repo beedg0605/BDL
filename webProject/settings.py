168b16600d14be44e5a886a0327f70cf1d30528c
beedg0605 <bealiongson05@gmail.com>
2024-09-17 13:02:03 +0800

    Modifications

--- webProject/settings.py
+++ webProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','BDL.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','BDL.onrender.com','bdl-pb75.onrender.com']
 
 
 # Application definition
